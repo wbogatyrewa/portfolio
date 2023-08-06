3557295fd23200b6ca7306c1d5af8f73fdfb44ff
wbogatyrewa <veronika01-01@mail.ru>
2023-08-06 20:49:29 +0500

    add container to gradient

---
+++ src/App.style.ts
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export const AnimatedGradientContainer = styled.div`
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Container } from "./components/Container";
 import { Main } from "./components/Main";
+import { AnimatedGradient } from "./components/AnimatedGradient";
+import { AnimatedGradientContainer } from "./App.style";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Container>
+        <AnimatedGradientContainer>
+          <AnimatedGradient />
+        </AnimatedGradientContainer>
+        <Header />
         <Main />
       </Container>
     </div>
