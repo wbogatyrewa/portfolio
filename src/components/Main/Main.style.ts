8016d34b9ac27621ca774ab1f849e809c05267e4
wbogatyrewa <veronika01-01@mail.ru>
2023-08-06 20:50:15 +0500

    rm gradient

--- src/components/Main/Main.style.ts
+++ src/components/Main/Main.style.ts
@@ -2,21 +2,10 @@ import styled from "@emotion/styled";
 
 export const MainContainer = styled.section`
   width: 100%;
-  position: relative;
   margin-top: 5vh;
   margin-bottom: 5vh;
 `;
 
-export const AnimatedGradientContainer = styled.div`
-  width: 100%;
-  position: absolute;
-  top: -15vw;
-  left: 0;
-  right: 0;
-  z-index: -1;
-`;
-
 export const HeadingContainer = styled.div`
   width: 100%;
-  position: absolute;
 `;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,18 +1,10 @@
 import React from "react";
-import { AnimatedGradient } from "../AnimatedGradient";
-import {
-  AnimatedGradientContainer,
-  HeadingContainer,
-  MainContainer,
-} from "./Main.style";
+import { HeadingContainer, MainContainer } from "./Main.style";
 import { H1 } from "../TextComponents";
 
 export const Main: () => JSX.Element = () => {
   return (
     <MainContainer>
-      <AnimatedGradientContainer>
-        <AnimatedGradient />
-      </AnimatedGradientContainer>
       <HeadingContainer>
         <H1>Hi,</H1>
         <H1>I'm Veronika,</H1>
