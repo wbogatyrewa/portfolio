71d544155d931d012e304d51a9ea1c2dce120142
wbogatyrewa <veronika01-01@mail.ru>
2023-08-06 20:05:36 +0500

    add props from gradient to container

--- src/components/AnimatedGradient/AnimatedGradient.style.ts
+++ src/components/AnimatedGradient/AnimatedGradient.style.ts
@@ -1,12 +1,6 @@
 import styled from "@emotion/styled";
 
 export const GradientSVG = styled.svg`
-  position: absolute;
-  top: -50vw;
-  left: 0;
-  z-index: -1;
-  height: 200vw;
-  width: 100vw;
   opacity: 0.9;
   --tw-blur: blur(15vw);
   filter: var(--tw-blur);
