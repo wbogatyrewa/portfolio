90f1152497c8fb1c7e674e6e5f690342e27838cb
wbogatyrewa <veronika01-01@mail.ru>
2023-08-06 20:22:07 +0500

    add Logo

---
+++ src/components/Logo/Logo.style.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+export const LogoSVG = styled.svg`
+  width: 144px;
+  height: 45px;
+
+  @media (min-width: 768px) {
+    width: 256px;
+    height: 45px;
+  }
+`;
