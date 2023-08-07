67217a99505062e8a94416cfd2d9b60f15adf3ce
wbogatyrewa <veronika01-01@mail.ru>
2023-08-07 20:50:00 +0500

    add H2 and P components

--- src/components/TextComponents/TextComponents.style.ts
+++ src/components/TextComponents/TextComponents.style.ts
@@ -11,3 +11,26 @@ export const H1 = styled.h1`
     font-size: 6rem;
   }
 `;
+
+export const H2 = styled.h2`
+  font-size: 1.8rem;
+
+  @media (min-width: 768px) {
+    font-size: 3.6rem;
+  }
+`;
+
+export const P = styled.p`
+  font-size: 1.2rem;
+  line-height: 40px;
+
+  @media (min-width: 768px) {
+    font-size: 1.8rem;
+    line-height: 60px;
+  }
+
+  @media (min-width: 2000px) {
+    font-size: 2.2rem;
+    line-height: 60px;
+  }
+`;
