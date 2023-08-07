6f0983805896419551806e69b6bab02bd455e450
wbogatyrewa <veronika01-01@mail.ru>
2023-08-07 21:58:18 +0500

    del media query

--- src/components/Main/Main.style.ts
+++ src/components/Main/Main.style.ts
@@ -3,10 +3,6 @@ import styled from "@emotion/styled";
 export const MainContainer = styled.section`
   width: 100%;
   margin-top: 10vh;
-
-  @media (min-width: 768px) {
-    margin-top: 10vh;
-  }
 `;
 
 export const HeadingContainer = styled.div`
