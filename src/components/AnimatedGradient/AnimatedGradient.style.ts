78648f9cd017144db241b3cfcd1c89d2833b2894
wbogatyrewa <veronika01-01@mail.ru>
2023-08-07 15:42:48 +0500

    change blur value

--- src/components/AnimatedGradient/AnimatedGradient.style.ts
+++ src/components/AnimatedGradient/AnimatedGradient.style.ts
@@ -2,8 +2,12 @@ import styled from "@emotion/styled";
 
 export const GradientSVG = styled.svg`
   opacity: 0.8;
-  --tw-blur: blur(5vw);
+  --tw-blur: blur(15vw);
   filter: var(--tw-blur);
+
+  @media (min-width: 992px) {
+    --tw-blur: blur(8vw);
+  }
 `;
 
 export const GradientPath = styled.path`
