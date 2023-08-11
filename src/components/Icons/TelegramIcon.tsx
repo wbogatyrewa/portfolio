3e3c66a3b93ccbf42f457a556d3cabe1661b084c
wbogatyrewa <veronika01-01@mail.ru>
2023-08-11 21:17:41 +0500

    change icons color

--- src/components/Icons/TelegramIcon.tsx
+++ src/components/Icons/TelegramIcon.tsx
@@ -11,17 +11,17 @@ export const TelegramIcon: () => JSX.Element = () => {
     >
       <path
         d="M87 38.6665L24.1667 60.4165L45.9167 67.6665M87 38.6665L45.9167 67.6665M87 38.6665L67.6667 89.4165L45.9167 67.6665"
-        stroke="white"
-        stroke-width="6"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        stroke="#F9F367"
+        strokeWidth="6"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M58 106.333C84.6945 106.333 106.333 84.6943 106.333 57.9998C106.333 31.3053 84.6945 9.6665 58 9.6665C31.3055 9.6665 9.66669 31.3053 9.66669 57.9998C9.66669 84.6943 31.3055 106.333 58 106.333Z"
-        stroke="white"
-        stroke-width="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        stroke="#F9F367"
+        strokeWidth="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
