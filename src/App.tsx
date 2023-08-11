a2e0d1226bcff139692f226e23e04509884c4f9b
wbogatyrewa <veronika01-01@mail.ru>
2023-08-11 21:15:21 +0500

    add Contact Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import { Container } from "./components/Container";
 import { Main } from "./components/Main";
 import { StarsBackground } from "./components/StarsBackground";
+import { AboutPage } from "./components/Pages/AboutPage";
+import { Contact } from "./components/Pages/ContactPage";
 
 function App() {
   return (
@@ -17,11 +19,25 @@ function App() {
             </Container>
           }
         />
-        <Route path="about" element={<Container></Container>} />
+        <Route
+          path="about"
+          element={
+            <Container>
+              <AboutPage />
+            </Container>
+          }
+        />
         <Route path="education" element={<Container></Container>} />
         <Route path="skills" element={<Container></Container>} />
         <Route path="work" element={<Container></Container>} />
-        <Route path="contact" element={<Container></Container>} />
+        <Route
+          path="contact"
+          element={
+            <Container>
+              <Contact />
+            </Container>
+          }
+        />
       </Routes>
     </>
   );
