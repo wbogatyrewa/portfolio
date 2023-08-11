4791516821af312f8897b7218d2b9d3c925221e0
wbogatyrewa <veronika01-01@mail.ru>
2023-08-11 23:28:57 +0500

    Updates

---
+++ precache-manifest.bdd4bf9e5b7643a110abe0d533aa30a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be9cfc25a831d11c30ea",
+    "url": "/portfolio/static/css/main.c5578edf.chunk.css"
+  },
+  {
+    "revision": "be9cfc25a831d11c30ea",
+    "url": "/portfolio/static/js/main.be9cfc25.chunk.js"
+  },
+  {
+    "revision": "3a79397ac1a1b3a66e79",
+    "url": "/portfolio/static/js/1.3a79397a.chunk.js"
+  },
+  {
+    "revision": "e9ed9161afbfee0332ab",
+    "url": "/portfolio/static/js/2.e9ed9161.chunk.js"
+  },
+  {
+    "revision": "7f7cae837392b1386668",
+    "url": "/portfolio/static/js/runtime~main.7f7cae83.js"
+  },
+  {
+    "revision": "54518e7dc5bc0dbf30caea89685b6d91",
+    "url": "/portfolio/index.html"
+  }
+];
