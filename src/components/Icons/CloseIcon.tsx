3b70e0fc70c6e4443ed87f23913111ae372ad579
wbogatyrewa <veronika01-01@mail.ru>
2023-08-11 20:52:39 +0500

    add close icon

---
+++ src/components/Icons/CloseIcon.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const CloseIcon: () => JSX.Element = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="36"
+      height="36"
+      viewBox="0 0 36 36"
+      fill="none"
+    >
+      <path
+        d="M18 18L28.5 28.5M18 18L7.5 7.5M18 18L7.5 28.5M18 18L28.5 7.5"
+        stroke="#F24E1E"
+        strokeWidth="8"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+};
--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -2,3 +2,4 @@ export * from "./MailIcon";
 export * from "./LinkedInIcon";
 export * from "./GithubIcon";
 export * from "./TelegramIcon";
+export * from "./CloseIcon";
