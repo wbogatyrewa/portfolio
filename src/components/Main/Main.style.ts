96300145c831abf1be90a72527624c44565842fe
wbogatyrewa <veronika01-01@mail.ru>
2023-08-11 17:43:50 +0500

    add Main

--- src/components/Main/Main.style.ts
+++ src/components/Main/Main.style.ts
@@ -2,9 +2,10 @@ import styled from "@emotion/styled";
 
 export const MainContainer = styled.section`
   width: 100%;
-  margin-top: 10vh;
 `;
 
 export const HeadingContainer = styled.div`
   width: 100%;
+  text-align: center;
+  margin-top: 40vh;
 `;
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,15 @@
 import React from "react";
-import { MainContainer } from "./Main.style";
+import { HeadingContainer, MainContainer } from "./Main.style";
+import { H1 } from "../TextComponents";
 
 export const Main: () => JSX.Element = () => {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      <HeadingContainer>
+        <H1>
+          welcome 2 <br /> wbogatyrewa’s <br /> website!!!
+        </H1>
+      </HeadingContainer>
+    </MainContainer>
+  );
 };
