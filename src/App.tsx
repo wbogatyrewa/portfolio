ebc1c243c37d066cb1bda3a65de65176395a6104
wbogatyrewa <veronika01-01@mail.ru>
2023-08-11 14:01:54 +0500

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import { Container } from "./components/Container";
 import { Main } from "./components/Main";
-import { AnimatedGradient } from "./components/AnimatedGradient";
-import {
-  AnimatedGradientContactContainer,
-  AnimatedGradientMainContainer,
-} from "./App.style";
-import { Header } from "./components/Header";
-import { About } from "./components/About";
-import { Contact } from "./components/Contact";
-import { Work } from "./components/Work";
 
 function App() {
   return (
-    <div className="App">
-      <Container>
-        <AnimatedGradientMainContainer>
-          <AnimatedGradient />
-        </AnimatedGradientMainContainer>
-        <Header />
-        <Main />
-        <About />
-        <Work />
-        <Contact />
-        <AnimatedGradientContactContainer>
-          <AnimatedGradient />
-        </AnimatedGradientContactContainer>
-      </Container>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Container>
+            <Main />
+          </Container>
+        }
+      />
+      <Route path="about" element={<Container></Container>} />
+      <Route path="education" element={<Container></Container>} />
+      <Route path="skills" element={<Container></Container>} />
+      <Route path="work" element={<Container></Container>} />
+      <Route path="contact" element={<Container></Container>} />
+    </Routes>
   );
 }
 
