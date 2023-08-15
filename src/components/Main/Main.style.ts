f336dd0be498670d9c4125a67240949cca77a7cb
wbogatyrewa <veronika01-01@mail.ru>
2023-08-15 20:36:22 +0500

    change vh to dvh

--- src/components/Main/Main.style.ts
+++ src/components/Main/Main.style.ts
@@ -8,7 +8,7 @@ export const HeadingContainer = styled.div`
   position: absolute;
   width: 100%;
   text-align: center;
-  margin-top: 35vh;
+  margin-top: 35dvh;
 `;
 
 export const PageButtonsContainer = styled.div`
@@ -16,62 +16,62 @@ export const PageButtonsContainer = styled.div`
 
   & > a:nth-of-type(1) {
     position: absolute;
-    top: 5vh;
+    top: 5dvh;
     left: 0;
   }
 
   & > a:nth-of-type(2) {
     position: absolute;
-    top: 2vh;
+    top: 2dvh;
     right: 0;
   }
 
   & > a:nth-of-type(3) {
     position: absolute;
-    top: 60vh;
+    top: 60dvh;
     right: 0;
   }
 
   & > a:nth-of-type(4) {
     position: absolute;
-    top: 55vh;
+    top: 55dvh;
     left: 0;
   }
 
   & > a:nth-of-type(5) {
     position: absolute;
-    top: 75vh;
+    top: 75dvh;
     left: 20vw;
   }
 
   @media (min-width: 768px) {
     & > a:nth-of-type(1) {
       position: absolute;
-      top: 5vh;
+      top: 5dvh;
       left: 34vw;
     }
 
     & > a:nth-of-type(2) {
       position: absolute;
-      top: 35vh;
+      top: 35dvh;
       right: 5vw;
     }
 
     & > a:nth-of-type(3) {
       position: absolute;
-      top: 70vh;
+      top: 70dvh;
       right: 25vw;
     }
 
     & > a:nth-of-type(4) {
       position: absolute;
-      top: 70vh;
+      top: 70dvh;
       left: 25vw;
     }
 
     & > a:nth-of-type(5) {
       position: absolute;
-      top: 35vh;
+      top: 35dvh;
       left: 5vw;
     }
   }
@@ -79,31 +79,31 @@ export const PageButtonsContainer = styled.div`
   @media (min-width: 1200px) {
     & > a:nth-of-type(1) {
       position: absolute;
-      top: 5vh;
+      top: 5dvh;
       left: 38vw;
     }
 
     & > a:nth-of-type(2) {
       position: absolute;
-      top: 35vh;
+      top: 35dvh;
       right: 10vw;
     }
 
     & > a:nth-of-type(3) {
       position: absolute;
-      top: 70vh;
+      top: 70dvh;
       right: 25vw;
     }
 
     & > a:nth-of-type(4) {
       position: absolute;
-      top: 70vh;
+      top: 70dvh;
       left: 25vw;
     }
 
     & > a:nth-of-type(5) {
       position: absolute;
-      top: 35vh;
+      top: 35dvh;
       left: 10vw;
     }
   }
